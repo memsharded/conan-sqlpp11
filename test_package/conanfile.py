f645339c744d01288ffbb8a3a04ff89ecfd78953
ericLemanissier <ericLemanissier@users.noreply.github.com>
2017-12-01 16:32:10 +0100

    applying bincrafters' test template

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,20 +1,14 @@
 from conans import ConanFile, CMake
 import os
 
-
-channel = os.getenv("CONAN_CHANNEL", "testing")
-username = os.getenv("CONAN_USERNAME", "memsharded")
-
-
 class sqlpp11TestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "sqlpp11/0.38@%s/%s" % (username, channel)
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def imports(self):
         self.copy("*.dll", "bin", "bin")
